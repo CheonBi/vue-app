c06780a23a520fb8f0c9938c9cace0f3bb89c11e
CheonBi <lets.noriter@gmail.com>
2023-12-12 15:36:07 +0900

    Update home design

--- src/views/HomeContent.vue
+++ src/views/HomeContent.vue
@@ -22,7 +22,10 @@
       <div class="box__wrapper right">
         <div class="box__base right__box">
           <div class="box__title right">
-            Title
+            <div class="icon"/>
+            <div class="title">
+              이것은 제목입니다.
+            </div>
           </div>
           <div class="box__content right">
             Box inner content
@@ -35,9 +38,11 @@
       <div class="box__wrapper left">
         <div class="box__base left__box">
           <div class="box__title left">
-            Title
+            <div class="icon"/>
+            <div class="title">
+              이것은 제목입니다.
+            </div>
           </div>
-
           <div class="box__content left">
             Box inner content
           </div>
@@ -47,7 +52,10 @@
       <div class="box__wrapper right">
         <div class="box__base right__box">
           <div class="box__title right">
-            Title
+            <div class="icon"/>
+            <div class="title">
+              이것은 제목입니다.
+            </div>
           </div>
           <div class="box__content right">
             Box inner content
