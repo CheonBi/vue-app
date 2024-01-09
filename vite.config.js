2b912edc11ecf7aefa3c6fedcbd77d03ca71cdf4
CheonBi <lets.noriter@gmail.com>
2024-01-09 10:50:36 +0900

    ignore cert(.pem, .key)

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,13 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import mkcert from 'vite-plugin-mkcert';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+
   ],
   resolve: {
     alias: {
@@ -15,8 +17,7 @@ export default defineConfig({
   },
 
   server:{
-    proxy:{
-    },
+    // https:true,
     port: 7173,
   },
 
